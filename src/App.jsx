5ae1d288f2939170e1729bd3cf66d1d3737b06c9
unknown <lalwadiyashubham123@gmail.com>
2024-06-10 21:00:00 +0530

    dummy data is added

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,80 @@ import CourseList from './components/Course/CourseList';
 import LoginlogoutContextProvider, { LoginlogoutContext } from './context/LoginlogoutContext';
 import Footer from './components/Layout/Footer';
 
+// Dummy courses data
+const dummyCourses = [
+  {
+      id: 1,
+      title: 'Introduction to Programming',
+      instructor: 'John Doe',
+      description: 'Learn the basics of programming.',
+      instructorEmail: 'john@example.com',
+      lessons: [
+          { title: 'Lesson 1', description: 'Introduction to programming languages.' },
+          { title: 'Lesson 2', description: 'Variables and data types.' },
+      ],
+      issueDate: '2022-06-01',
+      numLessons : 2
+  },
+  {
+      id: 2,
+      title: 'Web Development Fundamentals',
+      instructor: 'Jane Smith',
+      description: 'Explore the fundamentals of web development.',
+      instructorEmail: 'jane@example.com',
+      lessons: [
+          { title: 'Lesson 1', description: 'HTML basics.' },
+          { title: 'Lesson 2', description: 'CSS basics.' },
+          { title: 'Lesson 3', description: 'Introduction to JavaScript.' },
+      ],
+      issueDate: '2022-06-15',
+      numLessons : 3
+  },
+  {
+      id: 3,
+      title: 'Data Science Essentials',
+      instructor: 'Alice Johnson',
+      description: 'Learn essential concepts and techniques in data science.',
+      instructorEmail: 'alice@example.com',
+      lessons: [
+          { title: 'Lesson 1', description: 'Introduction to data science.' },
+          { title: 'Lesson 2', description: 'Data preprocessing and cleaning.' },
+          { title: 'Lesson 3', description: 'Exploratory data analysis.' },
+      ],
+      issueDate: '2022-07-01',
+      numLessons : 3
+  },
+  {
+      id: 4,
+      title: 'Machine Learning Basics',
+      instructor: 'Bob Williams',
+      description: 'Get started with machine learning algorithms and techniques.',
+      instructorEmail: 'bob@example.com',
+      lessons: [
+          { title: 'Lesson 1', description: 'Introduction to machine learning.' },
+          { title: 'Lesson 2', description: 'Supervised learning techniques.' },
+          { title: 'Lesson 3', description: 'Unsupervised learning techniques.' },
+      ],
+      issueDate: '2022-07-15',
+      numLessons : 3
+  },
+  {
+      id: 5,
+      title: 'Graphic Design Fundamentals',
+      instructor: 'Eva Brown',
+      description: 'Learn the basics of graphic design and visual communication.',
+      instructorEmail: 'eva@example.com',
+      lessons: [
+          { title: 'Lesson 1', description: 'Introduction to graphic design principles.' },
+          { title: 'Lesson 2', description: 'Color theory and typography.' },
+          { title: 'Lesson 3', description: 'Introduction to Adobe Photoshop.' },
+      ],
+      issueDate: '2022-08-01',
+      numLessons : 3
+  },
+];
+
+
 const App = () => {
   const [courses, setCourses] = useState([]);
   const [notifications, setNotifications] = useState([]);
@@ -23,9 +97,14 @@ const App = () => {
   useEffect(() => {
     // Retrieve course data from localStorage when component mounts
     const storedCourses = JSON.parse(localStorage.getItem('courses'));
-    if (storedCourses) {
+    if (storedCourses && storedCourses.length > 0) {
       setCourses(storedCourses);
+    } else {
+      // If no courses in local storage, store dummy courses
+      localStorage.setItem('courses', JSON.stringify(dummyCourses));
+      setCourses(dummyCourses);
     }
+
     const storedLogin = localStorage.getItem("login");
     if (storedLogin === "true") {
       setToggle(true);
--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -3,75 +3,7 @@ import { useEffect } from 'react';
 import CourseCard from '../components/Course/CourseCard';
 
 const Courses = ({ courses }) => {
-    useEffect(() => {
-        // Dummy course data
-        const dummyCourses = [
-            {
-                id: 1,
-                title: 'Introduction to Programming',
-                instructor: 'John Doe',
-                description: 'Learn the basics of programming.',
-                instructorEmail: 'john@example.com',
-                lessons: [
-                    { title: 'Lesson 1', description: 'Introduction to programming languages.' },
-                    { title: 'Lesson 2', description: 'Variables and data types.' },
-                ],
-            },
-            {
-                id: 2,
-                title: 'Web Development Fundamentals',
-                instructor: 'Jane Smith',
-                description: 'Explore the fundamentals of web development.',
-                instructorEmail: 'jane@example.com',
-                lessons: [
-                    { title: 'Lesson 1', description: 'HTML basics.' },
-                    { title: 'Lesson 2', description: 'CSS basics.' },
-                    { title: 'Lesson 3', description: 'Introduction to JavaScript.' },
-                ],
-            },
-            {
-                id: 3,
-                title: 'Data Science Essentials',
-                instructor: 'Alice Johnson',
-                description: 'Learn essential concepts and techniques in data science.',
-                instructorEmail: 'alice@example.com',
-                lessons: [
-                    { title: 'Lesson 1', description: 'Introduction to data science.' },
-                    { title: 'Lesson 2', description: 'Data preprocessing and cleaning.' },
-                    { title: 'Lesson 3', description: 'Exploratory data analysis.' },
-                ],
-            },
-            {
-                id: 4,
-                title: 'Machine Learning Basics',
-                instructor: 'Bob Williams',
-                description: 'Get started with machine learning algorithms and techniques.',
-                instructorEmail: 'bob@example.com',
-                lessons: [
-                    { title: 'Lesson 1', description: 'Introduction to machine learning.' },
-                    { title: 'Lesson 2', description: 'Supervised learning techniques.' },
-                    { title: 'Lesson 3', description: 'Unsupervised learning techniques.' },
-                ],
-            },
-            {
-                id: 5,
-                title: 'Graphic Design Fundamentals',
-                instructor: 'Eva Brown',
-                description: 'Learn the basics of graphic design and visual communication.',
-                instructorEmail: 'eva@example.com',
-                lessons: [
-                    { title: 'Lesson 1', description: 'Introduction to graphic design principles.' },
-                    { title: 'Lesson 2', description: 'Color theory and typography.' },
-                    { title: 'Lesson 3', description: 'Introduction to Adobe Photoshop.' },
-                ],
-            },
-        ];
-        
-
-        // Store dummy courses in local storage
-        localStorage.setItem('courses', JSON.stringify(dummyCourses));
-    }, []);
-
+   
     return (
         <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing="4">
             {courses?.map(course => (
